dc95db84ab9d559fe5801235638eb76d26891268
Stuart Leitch <stuart.leitch@leitches.net>
2024-11-09 22:18:44 +0000

    refactor routes and blog_post controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,18 +1,18 @@
 class BlogPostsController < ApplicationController
+
+  before_action :set_blog_post, except: [:index, :new, :create]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path, notice: "Blog post not found"
   end
-
+  
   def new
     @blog_post = BlogPost.new
   end
-
+  
   def create
     @blog_post = BlogPost.new(blog_post_params)
     if @blog_post.save
@@ -21,30 +21,33 @@ class BlogPostsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+  
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
-
+  
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
       render :edit, status: :unprocessable_entity
     end
-  end
+  end 
   
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
-
-private
-
-def blog_post_params
-  params.require(:blog_post).permit(:title, :body)
-end
-
+  
+  private
+  
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
+  
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, notice: "Blog post not found"
+  end
+  
 end
