1a2035ab8790f1fd4d8b7adcd9b2140e8643bb95
Stuart Leitch <stuart.leitch@leitches.net>
2024-11-09 23:38:29 +0000

    Add admin login and restriction for certain operations

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,6 @@
 class BlogPostsController < ApplicationController
 
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, except: [:index, :new, :create]
 
   def index
