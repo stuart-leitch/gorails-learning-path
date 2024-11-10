0328ea755c9e2f4868c378051d0a8b0cad1f1039
Stuart Leitch <stuart.leitch@leitches.net>
2024-11-10 13:04:40 +0000

    allow posts to be draft or scheduled

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,32 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  scope :draft, -> { where(published_at: nil)}
+  scope :published, -> { where("published_at <= ?", Time.current)}
+  scope :scheduled, -> { where("published_at > ?", Time.current)}
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
+
+  def status
+    if published? then
+      :published
+    elsif scheduled? then
+      :scheduled
+    else
+      :draft
+    end
+  end
+
 end
